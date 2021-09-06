1b8c016d2739079e5e938c99229d3bd91dbcbe30
WhenixD <whenix.developer@gmail.com>
2021-09-06 18:18:01 -0500

    Add FakeAPI

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,35 +7,49 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const API = 'http://localhost:3000/initialState';
 
-    <Categories title='Mi lista' />
-    <Carousel>
-      <CarouselItem />
-      <CarouselItem />
-      <CarouselItem />
-      <CarouselItem />
-    </Carousel>
+const App = () => {
+  const [videos, setVideos] = useState([]);
 
-    <Categories title='Tendencias' />
-    <Carousel>
-      <CarouselItem />
-      <CarouselItem />
-      <CarouselItem />
-      <CarouselItem />
-    </Carousel>
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
 
-    <Categories title='Originales' />
-    <Carousel>
-      <CarouselItem />
-      <CarouselItem />
-    </Carousel>
+  console.log(videos);
 
-    <Footer />
-  </div>
-);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+
+      <Categories title='Mi lista' />
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+
+      <Categories title='Tendencias' />
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+
+      <Categories title='Originales' />
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
