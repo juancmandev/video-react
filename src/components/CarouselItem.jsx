951fb0ef44b5108d5334f9b2fc0087c1ba2abdee
WhenixD <whenix.developer@gmail.com>
2021-09-09 19:14:13 -0500

    Redux

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,34 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setFavorite } from '../actions';
 import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/img/play-icon.png';
 import plusIcon from '../assets/img/plus-icon.png';
 
-const CarouselItem = ({ cover, title, year, contentRaiting, duration }) => (
-  <div className='carousel-item'>
-    <img className='carousel-item__img' src={cover} alt={title} />
-    <div className='carousel-item__details'>
-      <div>
-        <img
-          className='carousel-item__details--img'
-          src={playIcon}
-          alt='Play Icon'
-        />
-        <img
-          className='carousel-item__details--img'
-          src={plusIcon}
-          alt='Plus Icon'
-        />
+const CarouselItem = (props) => {
+  const { id, cover, title, year, contentRaiting, duration } = props;
+
+  const handleSetFavorite = () => {
+    props.setFavorite({
+      id,
+      cover,
+      title,
+      year,
+      contentRaiting,
+      duration,
+    });
+  };
+
+  return (
+    <div className='carousel-item'>
+      <img className='carousel-item__img' src={cover} alt={title} />
+      <div className='carousel-item__details'>
+        <div>
+          <img
+            className='carousel-item__details--img'
+            src={playIcon}
+            alt='Play Icon'
+          />
+          <img
+            className='carousel-item__details--img'
+            src={plusIcon}
+            alt='Plus Icon'
+            onClick={handleSetFavorite}
+          />
+        </div>
+        <p className='carousel-item__details--title'>{title}</p>
+        <p className='carousel-item__details--subtitle'>
+          {`${year} ${contentRaiting} ${duration}`}
+        </p>
       </div>
-      <p className='carousel-item__details--title'>{title}</p>
-      <p className='carousel-item__details--subtitle'>
-        {`${year} ${contentRaiting} ${duration}`}
-      </p>
     </div>
-  </div>
-);
+  );
+};
 
 CarouselItem.propTypes = {
+  id: PropTypes.number,
   cover: PropTypes.string,
   title: PropTypes.string,
   year: PropTypes.number,
@@ -36,4 +55,8 @@ CarouselItem.propTypes = {
   duration: PropTypes.number,
 };
 
-export default CarouselItem;
+const mapDispatchToProps = {
+  setFavorite,
+};
+
+export default connect(null, mapDispatchToProps)(CarouselItem);
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,45 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import '../assets/styles/App.scss';
-import useInitialState from '../hooks/useInitialState';
 
-const API = 'http://localhost:3000/initialState';
-
-const Home = () => {
-  const initialState = useInitialState(API);
-
-  return initialState.length === 0 ? (
-    <h1>Loading...</h1>
-  ) : (
+const Home = ({ myList, trends, originals }) => {
+  return (
     <React.Fragment>
       <Search />
-      {initialState.mylist.length > 0 && (
+      {myList.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
-            {initialState.mylist.map((item) => (
+            {myList.map((item) => (
               <CarouselItem key={item.id} {...item} />
             ))}
           </Carousel>
         </Categories>
       )}
 
-      {initialState.trends.length > 0 && (
+      {trends.length > 0 && (
         <Categories title='Tendencias'>
           <Carousel>
-            {initialState.trends.map((item) => (
+            {trends.map((item) => (
               <CarouselItem key={item.id} {...item} />
             ))}
           </Carousel>
         </Categories>
       )}
 
-      {initialState.originals.length > 0 && (
+      {originals.length > 0 && (
         <Categories title='Originales'>
           <Carousel>
-            {initialState.originals.map((item) => (
+            {originals.map((item) => (
               <CarouselItem key={item.id} {...item} />
             ))}
           </Carousel>
@@ -49,4 +43,12 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
